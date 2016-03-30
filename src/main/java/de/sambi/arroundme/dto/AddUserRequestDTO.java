21a9fdca89aae39b35102657d4f09fe03c6401d4
Berg-it <m.a.berguiga@gmail.Com>
2016-03-30 19:44:15 +0100

    seconde commit

---
+++ src/main/java/de/sambi/arroundme/dto/AddUserRequestDTO.java
@@ -0,0 +1,12 @@
+package de.sambi.arroundme.dto;
+
+import java.io.Serializable;
+
+public class AddUserRequestDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9113437510283104565L;
+
+}
